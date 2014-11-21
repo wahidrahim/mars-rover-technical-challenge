615365076ad4656a4e893c8fa32cc92dcde3f89e
Wahid Rahim <wahidrahim@gmail.com>
2014-11-21 13:08:22 -0500

    Update plateau.rb

--- plateau.rb
+++ plateau.rb
@@ -1,6 +1,6 @@
 class Plateau
 	attr_reader :grid
-	# creates a (x + 1)x(y + 1) 2D array
+	# creates a (x + 1)x(y + 1) 2D array initialized to 0
 	def initialize(x, y)
 		@grid = Array.new(x + 1) { Array.new(y + 1, 0) }
 	end
